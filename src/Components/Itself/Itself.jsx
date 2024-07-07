97c7684fda213dbca51ff33fb68df3f8a6956275
Ankush Jha <67179973+CR4ZED@users.noreply.github.com>
2024-07-07 15:38:20 +0530

    Added header, footer, and implemented responsive layout

--- src/Components/Itself/Itself.jsx
+++ src/Components/Itself/Itself.jsx
@@ -1,53 +1,51 @@
-
-
-import React, { useState } from 'react';
-import "./Itself.css"
-import Selectbox from '../Selectbox/Selectbox';
-import Typeable from '../Typeable/Typeable';
-import Cashtype from '../Cashtype/Cashtype';
-import Selectable from '../Selectable/Selectable';
-import SearchButton from '../SearchButton/SearchButton';
-import  {markaOptions, colorOptions, cityOptions, banOptions} from '../../utils/Data';
-
-
+import React, { useState } from "react";
+import styles from "./Itself.module.css";
+import Selectbox from "../Selectbox/Selectbox";
+import Typeable from "../Typeable/Typeable";
+import Cashtype from "../Cashtype/Cashtype";
+import Selectable from "../Selectable/Selectable";
+import SearchButton from "../SearchButton/SearchButton";
+import {
+  markaOptions,
+  colorOptions,
+  cityOptions,
+  banOptions,
+} from "../../utils/Data";
 
 function Itself() {
-
-    return(
-        <div className='the-search-bar'>
-            <h1 className='main-title'>Avtomibillərin icarəsi</h1>
-            <div className='marka-box-container'>
-                <div className='select-box'>
-                  <Selectbox type="marka" options={markaOptions} title="Marka"/>
-                </div>
-                <div className='select-box'>
-                  <Selectbox type="model" options={cityOptions} title="Model"/>
-                </div>
-                <div className='select-box'>
-                  <Selectbox type="city" options={cityOptions} title="Şəhər"/>
-                </div>
-                <div className='select-box'>
-                  <Typeable />
-                </div>
-                <div className='select-box'>
-                  <Selectbox type="ban" options={banOptions} title="Ban növü"/>
-                </div>
-                <div className='select-box'>
-                  <Cashtype />
-                </div>
-                <div className='select-box'>
-                  <Selectbox type="color" options={colorOptions} title="Rəng"/>
-                </div>
-                <div className='select-box'>
-                  <Selectable />
-                </div>
-                <div className='select-box'>
-                  <SearchButton />
-                </div>
-                
-                
-            </div>
+  return (
+    <div className={styles.searchSection}>
+      <h1 className={styles.mainTitle}>Avtomibillərin icarəsi</h1>
+      <div className={styles.markaBoxContainer}>
+        <div className={styles.selectBox}>
+          <Selectbox type="marka" options={markaOptions} title="Marka" />
         </div>
-    )
+        <div className={styles.selectBox}>
+          <Selectbox type="model" options={cityOptions} title="Model" />
+        </div>
+        <div className={styles.selectBox}>
+          <Selectbox type="city" options={cityOptions} title="Şəhər" />
+        </div>
+        <div className={styles.selectBox}>
+          <Typeable />
+        </div>
+        <div className={styles.selectBox}>
+          <Selectbox type="ban" options={banOptions} title="Ban növü" />
+        </div>
+        <div className={styles.selectBox}>
+          <Cashtype />
+        </div>
+        <div className={styles.selectBox}>
+          <Selectbox type="color" options={colorOptions} title="Rəng" />
+        </div>
+        <div className={styles.selectBox}>
+          <Selectable />
+        </div>
+        <div className={styles.selectBox}>
+          <SearchButton />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Itself;
